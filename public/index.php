e235678a90f8fcb741bb71ccf38ad22dbc73d507
MrQwerty9 <sholihin.ok1998@gmail.com>
2020-03-24 22:26:29 +0700

    line bot

---
+++ public/index.php
@@ -0,0 +1,57 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+ 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+ 
+use \LINE\LINEBot;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+ 
+$pass_signature = true;
+ 
+// set LINE channel_access_token and channel_secret
+$channel_access_token = "BmRuAuS0LXGKWLH5yTOgPPK0xBH72J9eAt3PB/0Iu5ZD+BMTx/3B9gV8rceCY5XSIibdOaVHM/LmFJupUgXm11GtzlrzNainf7c1w3Pz6fInfxOTX5DFMRJWPEgmJgg7O8E2tJ71yajjYe0Xuy40PwdB04t89/1O/w1cDnyilFU=";
+$channel_secret = "779e53d220743fc7eacf2a39d3e5b9c6";
+ 
+// inisiasi objek bot
+$httpClient = new CurlHTTPClient($channel_access_token);
+$bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+ 
+$app = AppFactory::create();
+$app->setBasePath("/public");
+ 
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello World!");
+    return $response;
+});
+ 
+// buat route untuk webhook
+$app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
+    // get request body and line signature header
+    $body = $request->getBody();
+    $signature = $request->getHeaderLine('HTTP_X_LINE_SIGNATURE');
+ 
+    // log body and signature
+    file_put_contents('php://stderr', 'Body: ' . $body);
+ 
+    if ($pass_signature === false) {
+        // is LINE_SIGNATURE exists in request header?
+        if (empty($signature)) {
+            return $response->withStatus(400, 'Signature not set');
+        }
+ 
+        // is this request comes from LINE?
+        if (!SignatureValidator::validateSignature($body, $channel_secret, $signature)) {
+            return $response->withStatus(400, 'Invalid signature');
+        }
+    }
+    
+// kode aplikasi nanti disini
+ 
+});
+$app->run();
